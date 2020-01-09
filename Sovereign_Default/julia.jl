780cecda88caaa412605ac67491ed4a2de651142
Mason Protter <mason.protter@icloud.com>
2020-01-09 11:25:46 -0700

    Update julia.jl

--- Sovereign_Default/julia.jl
+++ Sovereign_Default/julia.jl
@@ -1,9 +1,8 @@
-using DelimitedFiles, Statistics
+using DelimitedFiles, Statistics, Strided
 
 # Load grid for log(y) and transition matrix
 const logy_grid = readdlm("logy_grid.txt")[:]
 const Py = readdlm("P.txt")
-
 
 function main(nB=351, repeats=500)
     β = .953
@@ -12,7 +11,7 @@ function main(nB=351, repeats=500)
     θ = 0.282
     ny = size(logy_grid, 1)
 
-    Bgrid = LinRange(-.45, .45, nB)
+    Bgrid = collect(range(-.45, .45, length=nB))
     ygrid = exp.(logy_grid)
 
     ymean = mean(ygrid) .+ 0 * ygrid
@@ -29,46 +28,35 @@ function main(nB=351, repeats=500)
 
     zero_ind = Int(ceil(nB / 2))
 
-    u(c, γ) = c^(1 - γ) / (1 - γ)
+    @fastmath u(c, γ) = c^(1.0 - γ) / (1.0 - γ)
+    relu(x) = x < 1e-14 ? 1e-14 : x
 
-    t0 = time()
     function iterate(V, Vc, Vd, Q)
-        EV = Py * V
-        EVd = Py * Vd
-        EVc = Py * Vc
+        @fastmath begin 
+            EV = Py * V
+            EVd = Py * Vd
+            EVc = Py * Vc
 
-        Vd_target = u.(def_y, γ) + β * (θ * EVc[:, zero_ind] + (1 - θ) * EVd[:])
-        Vd_target = reshape(Vd_target, (ny, 1))
+            Vd_target = @strided u.(def_y, γ) + β * (θ * EVc[:, zero_ind] + (1.0 - θ) * EVd)
+            Vd_target = reshape(Vd_target, (ny, 1))
 
-        Qnext = reshape(Q, (ny, 1, nB))
+            Qnext = reshape(Q, (ny, 1, nB))
+            c =  @strided relu.(y .- Qnext .* Bnext .+ B)
+            #map!(x->max(x, 1e-14), c, c)
+            EV = reshape(EV, (ny, 1, nB))
+            m = @strided u.(c, γ) .+ β .* EV
+            
+            Vc_target = reshape(maximum(m, dims=3), (ny, nB))
 
-        m = Array{Float64,3}(undef, ny, nB, nB)
-        @inbounds for k = 1:nB
-            Bk = Bgrid[k]
-            for j = 1:nB
-                Bj = Bgrid[j]
-                @simd for i = 1:ny
-                    c = ygrid[i] - Q[i,k] * Bk + Bj
-                    c = max(c, 1e-14)
-                    m[i, j, k] = u(c, γ) + β * EV[i,j]
-                end
-            end
+            Vd_compat = Vd * ones(1, nB)
+            default_states = float(Vd_compat .> Vc)
+            default_prob = Py * default_states
+            Q_target = @strided (1. .- default_prob) ./ (1 + r)
+
+            V_target = @strided max.(Vc, Vd_compat)
+
+            return V_target, Vc_target, Vd_target, Q_target
         end
-        # c = @. y - Qnext * Bnext + B
-        # map!(x->max(x, 1e-14), c, c)
-        # EV = reshape(EV, (ny, 1, nB))
-        # m =  @. u(c, γ) + β * EV
-        Vc_target = reshape(maximum(m, dims=3), (ny, nB))
-
-        Vd_compat = Vd * ones(1, nB)
-        default_states = float(Vd_compat .> Vc)
-        default_prob = Py * default_states
-        Q_target = (1. .- default_prob) ./ (1 + r)
-
-        V_target = max.(Vc, Vd_compat)
-
-        return V_target, Vc_target, Vd_target, Q_target
-
     end
 
     iterate(V, Vc, Vd, Q)  # warmup
@@ -77,8 +65,10 @@ function main(nB=351, repeats=500)
         V, Vc, Vd, Q = iterate(V, Vc, Vd, Q)
     end
     t1 = time()
+    
     out = (t1 - t0) / repeats
-
 end
 
-print(1000 * main(1351, 10))
+for sz in [151, 351, 551, 751, 951, 1151, 1351, 1551]
+    println(sz, ":  ", 1000 * main(sz, 10))
+end
